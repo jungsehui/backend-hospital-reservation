ac4c1d12d277803b66fbce44b9e0ab017f104064
jungsehui <jungsehui@naver.com>
2025-03-25 14:37:11 +0900

    refactor: 롬복 사용 대신 생성자 주입으로 변환, 예약 취소 반환 타입을 boolean으로 변경

--- src/main/java/com/example/hospitalreservation/service/ReservationService.java
+++ src/main/java/com/example/hospitalreservation/service/ReservationService.java
@@ -9,12 +9,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 // TODO : 서비스 레이어에서 필요한 어노테이션을 작성해주세요.
-@RequiredArgsConstructor
 @Service
 public class ReservationService {
 
     // TODO : 주입 받아야 객체를 작성해주세요.
     private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
 
     // TODO : 모든 예약 리스트를 조회하는 코드를 작성해주세요.
     public List<Reservation> getAllReservations() {
@@ -23,20 +26,16 @@ public class ReservationService {
 
     // TODO : 새로운 예약을 생성하는 코드를 작성해주세요.
     public Reservation createReservation(Long doctorId, Long patientId, LocalDateTime reservationTime) {
-        if (doctorId <= 0) {
-            throw new IllegalArgumentException("ID 값은 0 또는 음수가 될 수 없습니다. ID: %d".formatted(doctorId));
-        }
-
-        if (patientId <= 0) {
-            throw new IllegalArgumentException("ID 값은 0 또는 음수가 될 수 없습니다. ID: %d".formatted(patientId));
-        }
-
         Reservation reservation = new Reservation(null, doctorId, patientId, reservationTime);
         return reservationRepository.save(reservation);
     }
 
     // TODO : 예약을 취소하는 코드를 작성해주세요.
-    public void cancelReservation(Long id) {
-        reservationRepository.deleteById(id);
+    public boolean cancelReservation(Long id) {
+        if (reservationRepository.deleteById(id)) {
+            return true;
+        }
+
+        return false;
     }
 }
